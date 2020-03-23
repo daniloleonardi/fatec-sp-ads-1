2468fc98e1ac31b9cb89d6f224295c406914109f
daniloleonardi <62550528+daniloleonardi@users.noreply.github.com>
2020-03-23 10:40:51 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  Atividade 11
+//  Rodízio
+//  Created by Danilo Leonardi on 22/03/20.
+//  Copyright © 2020 Danilo Leonardi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int placa, n;
+    printf("\nDescubra seu dia de rodizio. \n\nDigite o numero da placa: ");
+    scanf("%d", &placa);
+    n = placa % 10;
+    switch(n){
+    
+        case 1:
+        case 2: printf("Segunda-feira"); break;
+        case 3:
+        case 4: printf("Terca-feira"); break;
+        case 5:
+        case 6: printf("Quarta-feira"); break;
+        case 7:
+        case 8: printf("Quinta-feira"); break;
+        default: printf("Sexta-feira"); break;
+    }
+        printf("\n\n");
+    return 0;
+
+    }
